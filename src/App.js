f00a40a162f4fee7298268dfebf4dd786fedeaf7
Rohit Londhe <rohitlondhe446@gmail.com>
2022-12-19 16:09:19 +0530

    finish

--- src/App.js
+++ src/App.js
@@ -1,88 +1,90 @@
 import { useState } from "react";
-import InputForm from "./components/InputForm";
+import FormInput from "./components/InputForm";
 
-
-function App() {
+const App = () => {
   const [values, setValues] = useState({
-    Username: '',
-    Email: '',
-    BirthDay: "",
-    Password: '',
-    ConfirmPassword: ""
-  })
+    username: "",
+    email: "",
+    birthday: "",
+    password: "",
+    confirmPassword: "",
+  });
 
   const inputs = [
     {
-      id: '1',
-      name: 'Username',
-      type: 'text',
-      placeholder: 'Username',
-      errorMessage: "Username should be between 3 to 16 charecters and should not include any special charecter",
-      label: 'Username',
-      require: 'true'
+      id: 1,
+      name: "username",
+      type: "text",
+      placeholder: "Username",
+      errorMessage:
+        "Username should be 3-16 characters and shouldn't include any special character!",
+      label: "Username",
+      pattern: "^[A-Za-z0-9]{3,16}$",
+      required: true,
     },
     {
-      id: '2',
-      name: 'Email',
-      type: 'text',
-      placeholder: 'Email',
-      errorMessage: "Please provide a valid email address",
-      label: 'Email',
-      require: 'true'
+      id: 2,
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+      errorMessage: "It should be a valid email address!",
+      label: "Email",
+      required: true,
     },
     {
-      id: '3',
-      name: 'BirthDay',
-      type: 'date',
-      placeholder: 'BirthDay',
-      label: 'BirthDay',
-      require: 'true'
+      id: 3,
+      name: "birthday",
+      type: "date",
+      placeholder: "Birthday",
+      label: "Birthday",
     },
     {
-      id: '4',
-      name: 'Password',
-      type: 'password',
-      placeholder: 'Password',
-      errorMessage: "Password should be between 8 to 20 characters with atleast 1 Uppercase letter and one special character",
-      label: 'Password'
+      id: 4,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      errorMessage:
+        "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
+      label: "Password",
+      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+      required: true,
     },
     {
-      id: '5',
-      name: 'ConfirmPassword',
-      type: 'password',
-      placeholder: 'ConfirmPassword',
-      errorMessage: "Password don't match",
-      label: 'ConfirmPassword',
-      require: 'true'
-    }
-  ]
+      id: 5,
+      name: "confirmPassword",
+      type: "password",
+      placeholder: "Confirm Password",
+      errorMessage: "Passwords don't match!",
+      label: "Confirm Password",
+      pattern: values.password,
+      required: true,
+    },
+  ];
 
   const handleSubmit = (e) => {
     e.preventDefault();
-  }
+  };
 
   const onChange = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value })
-  }
-
-  console.log(values)
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
 
   return (
     <div className="App flex items-center justify-center h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
-
-      <form className="bg-white p-3 my-12 rounded-lg flex flex-col items-center justify-center" onSubmit={handleSubmit}>
+      <form className="bg-white p-3 my-12 w-96 rounded-lg flex flex-col items-center justify-center" onSubmit={handleSubmit}>
         <h1 className="text-3xl text-indigo-600 font-extrabold">Register</h1>
         {inputs.map((input) => (
-          <InputForm key={input.id} {...input} value={values[input.name]} onChange={onChange} />
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
         ))}
-
-        <button
-          class="py-2 my-6 px-4 bg-transparent text-indigo-600 font-semibold border border-indigo-600 rounded hover:bg-indigo-600 hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0"
-        >Register</button>
+        <button className="py-2 my-6 px-4 bg-transparent text-indigo-600 font-semibold border border-indigo-600 rounded hover:bg-indigo-600 hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0">Submit</button>
       </form>
-
     </div>
   );
-}
+};
 
 export default App;
--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import { useState } from "react";
+import "./input.css";
 
-const InputForm = (props) => {
+const FormInput = (props) => {
+  const [focused, setFocused] = useState(false);
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
 
-  const {label, errorMessage, onChange, id, ...inputProps} = props;
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
+
   return (
-    <div className='inputform flex flex-col w-96'>
-      <label className='text-2xs text-gray-600'>{label}</label>
-      <input className='p-3 m-2 rounded-lg border-solid border-2 border-gray-300 ' {...inputProps} onChange={onChange} />
-      <span className='text-xs p-3 text-red-600 hidden'>{errorMessage}</span>
+    <div className="flex flex-col w-80">
+      <label className="text-2xs text-gray-600">{label}</label>
+      <input className="p-3 m-2 rounded-xl border border-gray-300"
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        onFocus={() =>
+          inputProps.name === "confirmPassword" && setFocused(true)
+        }
+        focused={focused.toString()}
+      />
+      <span className="text-sm p-3 text-red-500 hidden invalid:border-blue-500">{errorMessage}</span>
     </div>
-  )
-}
+  );
+};
 
-export default InputForm;
+export default FormInput;
